b27e84258af46f1b8f6ed991e6ff4ee5b7a1f4fa
Mike Sheets <wmichaelsheets@gmx.com>
2024-08-15 14:37:25 -0400

    all code working through product prices.

--- scripts/Products.js
+++ scripts/Products.js
@@ -2,11 +2,31 @@ import { getProducts } from "./database.js"
 
 const products = getProducts()
 
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const productClicked = clickEvent.target
+
+        if (productClicked.dataset.type === "product") {
+            window.alert(`The ${productClicked.dataset.name} costs ${productClicked.dataset.price}`)
+        }
+    }
+)
+
+
+
+
+
 export const Products = () => {
     let html = "<ul>"
 
     for (const product of products) {
-        html += `<li>${product.name}</li>`
+        html += `<li data-id="${product.id}"
+                  data-name="${product.name}"
+                  data-price="${product.price}"
+                  data-type="product"
+                  >${product.name}
+                  </li>`
     }
 
     html += "</ul>"
